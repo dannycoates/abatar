2b25291abedcfd21070abea45cb4e88f664deac9
Danny Coates <dannycoates@gmail.com>
2015-01-19 12:55:21 -0800

    added ab.now()

--- ab.js
+++ ab.js
@@ -2,31 +2,36 @@ var Experiment = require('./experiment')
 var ExperimentIndex = require('./experiment_index')
 var util = require('./util')
 
-function AB() {
-  this.experiments = new ExperimentIndex()
+function AB(options) {
+  var experiments = options.experiments || []
+  var enrolled = options.enrolled || []
+
+  this.defaults = options.defaults || {}
   this.enrolled = new ExperimentIndex()
-  this.subject = {}
-  this.defaults = {}
+  this.experiments = new ExperimentIndex()
+  this.localNow = options.localNow || Date.now()
+  this.remoteNow = options.remoteNow || this.localNow
+  this.subject = options.subject || {}
+
+  for (var i = 0; i < experiments.length; i++) {
+    var x = experiments[i]
+    x.defaults = this.defaults
+    this.experiments.add(new Experiment(x))
+  }
+  for (var i = 0; i < enrolled.length; i++) {
+    this.enroll(this.experiments.get(enrolled[i]))
+  }
 }
 
 AB.Experiment = Experiment
 
-AB.create = function (experiments, enrolled, defaults, now) {
-  if (!Array.isArray(experiments)) { throw new Error('create only accepts an array') }
-  enrolled = enrolled || []
-  now = now || Date.now()
+AB.create = function (options) {
+  return new AB(options)
+}
 
-  var ab = new AB()
-  ab.defaults = defaults || {}
-  for (var i = 0; i < experiments.length; i++) {
-    var x = experiments[i]
-    x.defaults = this.defaults
-    ab.add(new Experiment(x))
-  }
-  for (var i = 0; i < enrolled.length; i++) {
-    ab.enroll(ab.experiments.get(enrolled[i]))
-  }
-  return ab
+AB.prototype.now = function () {
+  var delta = this.remoteNow - this.localNow
+  return Date.now() + delta
 }
 
 AB.prototype.add = function (experiment) {
@@ -35,7 +40,7 @@ AB.prototype.add = function (experiment) {
 }
 
 AB.prototype.enroll = function (experiment, now) {
-  now = now || Date.now()
+  now = now || this.now()
   if (typeof(experiment) === 'string') {
     experiment = this.experiments.get(experiment)
   }
@@ -58,7 +63,7 @@ AB.prototype.enroll = function (experiment, now) {
 
 /*/
 AB.prototype.choose = function (variable, subject, now) {
-  now = now || Date.now()
+  now = now || this.now()
   var s = util.merge(this.subject, subject)
   var x = this.enrolled.getFirstLive(variable, now) ||
           this.experiments.getFirstEligible(variable, s, this.enrolled, now) ||
--- test/ab_tests.js
+++ test/ab_tests.js
@@ -4,17 +4,18 @@ var AB = require('../')
 test(
   'choose',
   function (t) {
-    var ab = AB.create([
-      {
-        name: 'foo',
-        startDate: (new Date()).toISOString(),
-        independentVariables: {
-          x: 1
-        },
-        eligibilityFunction: function () { return true },
-        groupingFunction: function () { return { x: 2 } }
-      }
-    ])
+    var ab = AB.create({
+      experiments: [
+        {
+          name: 'foo',
+          startDate: (new Date()).toISOString(),
+          independentVariables: {
+            x: 1
+          },
+          eligibilityFunction: function () { return true },
+          groupingFunction: function () { return { x: 2 } }
+        }
+      ]})
     t.equal(ab.choose('x'), 2, 'choice came from groupingFunction')
     t.end()
   }
@@ -23,8 +24,8 @@ test(
 test(
   'choose returns the default when no experiment sets the variable',
   function (t) {
-    var ab = AB.create(
-      [{
+    var ab = AB.create({
+      experiments: [{
         name: 'foo',
         startDate: (new Date()).toISOString(),
         independentVariables: {
@@ -33,12 +34,26 @@ test(
         eligibilityFunction: function () { return true },
         groupingFunction: function () { return { x: 2 } }
       }],
-      [],
-      {
+      defaults: {
         z: 1
       }
-    )
+    })
     t.equal(ab.choose('z'), 1, 'choice came from defaults')
+    t.end()
+  }
+)
+
+test(
+  'ab.now is relative to remoteNow',
+  function (t) {
+    var now = Date.now()
+    var remoteNow = (new Date('2015-01-01')).valueOf()
+    var ab = AB.create({
+      remoteNow: remoteNow,
+      localNow: now
+    })
+    var d = ab.now() - remoteNow
+    t.ok(d < 10 && d >= 0, 'close enough')
     t.end()
   }
 )
